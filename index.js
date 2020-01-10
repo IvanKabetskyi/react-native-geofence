b7c32c97c7e42ea4751ed88b3c2ea124b3daa8c4
Ivan <ivan@Admins-Mac-mini.local>
2020-01-10 15:23:17 +0200

    UPDATE: index, add there function

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, NativeAppEventEmitter, AppRegistry } from 'react-native';
 
 const { Geofence } = NativeModules;
 
-export default Geofence;
+const geofenceEventEmitter = new NativeAppEventEmitter(Geofence);
+
+const Events = {
+	EXIT: 'onExit',
+	ENTER: 'onEnter'
+}
+
+export {
+	Events
+}
+
+const HeadlessGeofenceEventTask = async (({event, ids}) => {
+	console.log(event ,ids);
+	geofenceEventEmitter.emit(event, ids);
+});
+
+AppRegistry.registerHeadlessTask('OnGeofenceEvent', () => HeadlessGeofenceEventTask);
+
+
+export default {
+	add: geofenceProperites => {
+		return new Promise((resolve, reject) => {
+			Geofence.add(geofenceProperites)
+				.then(id => resolve(id))
+				.catch(e => reject(e))
+		})
+	}
+};
